d1750838c3010fed02ede95b763e24f40d177ee8
Gilson Alves <gilsonalvesdesouza@hotmail.com>
2022-10-24 18:17:47 -0300

    finalizado 04

--- 04/index.js
+++ 04/index.js
@@ -1,6 +1,19 @@
-
-
 let tempoParaAlarmarEmSegundos = 10;
 let tempoAlarmandoEmSegundos = 5;
 
 
+let countTempoAlarmado = 0;
+const alarm = (tempoAlarmandoEmSegundos) => {
+  const clearTempoAlarmado = setInterval(() => {
+    console.log("Beep beep!");
+    countTempoAlarmado++;
+    if (countTempoAlarmado >= tempoAlarmandoEmSegundos) {
+      clearInterval(clearTempoAlarmado);
+      console.log("Alarme finalizado!");
+    }
+
+  }, 1000);
+};
+console.log(`Timer iniciado, disparando em ${tempoParaAlarmarEmSegundos} segundos`);
+
+setTimeout(alarm, tempoParaAlarmarEmSegundos * 1000, tempoAlarmandoEmSegundos);
